b866f205ad58c3f6405916c4d421e6b70636edfc
i388267 <gijsser12@gmail.com>
2020-06-16 13:08:08 +0200

    working with c#

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,42 @@
 
 Controller controller;
 
+
+void testJsonAdd()
+{
+  DynamicJsonDocument doc(1024);
+  DynamicJsonDocument doc2(1024);
+  DynamicJsonDocument doc3(1024);
+  DynamicJsonDocument doc4(1024);
+
+  char json[] = "{\"type\":1,\"sensNr\":1,\"sensAdd\":104}";
+  char json2[] = "{\"type\":2,\"sensNr\":1,\"instNr\":1,\"reqB\":59,\"bNr\":0,\"bVal\":0,\"BRet\":14}";
+  //char json2[] = "{\"type\":2,\"sensNr\":1,\"instNr\":1,\"reqB\":59,\"BRet\":14}";
+  char json3[] = "{\"type\":4,\"sensAdd\":104,\"reqB\":107,\"bNr\":2,\"bVal\":1}";
+  char json4[] = "{\"type\":4,\"sensAdd\":104,\"reqB\":107,\"bNr\":6,\"bVal\":1}";
+  
+
+  deserializeJson(doc, json);
+  controller.parseData(doc);
+
+  delay(100);
+  
+  deserializeJson(doc2, json2);
+  controller.parseData(doc2);
+
+  delay(100);
+  
+  deserializeJson(doc3, json3);
+  controller.parseData(doc3);
+
+  deserializeJson(doc4, json4);
+  controller.parseData(doc4);
+}
+
 void setup() 
 {
   controller.begin();
+  testJsonAdd();
 }
 
 void loop() 
@@ -20,7 +53,6 @@ void loop()
     
     lastMillis = millis();
   }
-
 
   delay(100);
 
